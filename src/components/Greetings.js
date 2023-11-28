e61427d9229ffc08ba892bc082322a36d4bc1763
Toyyib <hamzatbolaji648@yahoo.com>
2023-11-28 22:41:56 +0100

    develop

---
+++ src/components/Greetings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = ({ greeting }) => {
+  return (
+    <div>
+      <h2>Greeting</h2>
+      <p>{greeting}</p>
+    </div>
+  );
+};
+
+export default Greeting;
